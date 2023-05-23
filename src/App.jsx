739bc5dd5e824525c8c494b34cce87de7e84fce8
Mosijchuk-Rostislav <123548936+Mosijchuk-Rostislav@users.noreply.github.com>
2023-05-23 21:05:57 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,15 @@ function App() {
   const [sortType, setSortType] = useState("");
   const [sortDirection, setSortDirection] = useState("asc");
   const [reverseOrder, setReverseOrder] = useState(false);
+  const [showRegions, setShowRegions] = useState(false);
+  const [selectedRegion, setSelectedRegion] = useState("");
+  const [selectedSubregion, setSelectedSubregion] = useState("");
+  const [resetSort, setResetSort] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
+        const response = await fetch("http://46.101.96.179/all");
         const data = await response.json();
         setCountries(data);
       } catch (error) {
@@ -29,8 +33,21 @@ function App() {
   const start = (page - 1) * countriesPerPage;
   const end = start + countriesPerPage;
   const filteredCountries = countries.filter((country) => {
-    return country.name.common.toLowerCase().includes(searchTerm.toLowerCase());
+    return (
+      country.name.common.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      (selectedRegion === "" || country.region === selectedRegion) &&
+      (selectedSubregion === "" || country.subregion === selectedSubregion)
+    );
   });
+
+  if (resetSort) {
+    setResetSort(false);
+    filteredCountries.sort((a, b) =>
+      reverseOrder
+        ? b.name.common.localeCompare(a.name.common)
+        : a.name.common.localeCompare(b.name.common)
+    );
+  }
 
   if (sortType === "a-z") {
     filteredCountries.sort((a, b) =>
@@ -38,18 +55,19 @@ function App() {
         ? a.name.common.localeCompare(b.name.common)
         : b.name.common.localeCompare(a.name.common)
     );
-  } else if (sortType === "region") {
-    filteredCountries.sort((a, b) =>
-      sortDirection === "asc"
-        ? a.region.localeCompare(b.region)
-        : b.region.localeCompare(a.region)
-    );
   } else if (reverseOrder) {
     filteredCountries.reverse();
   }
 
   const currentCountries = filteredCountries.slice(start, end);
   const renderCountries = () => {
+    if (filteredCountries.length === 0) {
+      return (
+        <p>
+          No countries found for the selected region and subregion combination.
+        </p>
+      );
+    }
     return currentCountries.map((country, index) => (
       <Country
         country={country}
@@ -61,10 +79,83 @@ function App() {
     ));
   };
 
+  function RegionList() {
+    const regions = Array.from(
+      new Set(countries.map((country) => country.region))
+    ).filter((region) => region !== "");
+
+    const subregions = Array.from(
+      new Set(
+        countries
+          .filter((country) => country.region === selectedRegion)
+          .map((country) => country.subregion)
+      )
+    ).filter((subregion) => subregion !== "");
+
+    return (
+      <div className="region">
+        <ul className="region-list">
+          <h2>Region:</h2>
+          {regions.map((region, index) => (
+            <button key={index} onClick={() => handleRegionSelect(region)}>
+              {region}
+            </button>
+          ))}
+        </ul>
+        {selectedRegion && (
+          <ul className="subregion-list">
+            <h2>Subregion:</h2>
+            {subregions.map((subregion, index) => (
+              <button
+                key={index}
+                onClick={() => setSelectedSubregion(subregion)}
+                className={selectedSubregion === subregion ? "active" : ""}
+              >
+                {subregion}
+              </button>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+
   const renderPagination = () => {
     const totalPages = Math.ceil(filteredCountries.length / countriesPerPage);
     const pages = [];
-    for (let i = 1; i <= totalPages; i++) {
+  
+    if (totalPages <= 1) {
+      return null; // Don't render pagination if there's only one page
+    }
+  
+    const showEllipsis = totalPages > 5;
+  
+    if (page > 1) {
+      pages.push(
+        <button key="prev" onClick={() => setPage(page - 1)}>
+          Back
+        </button>
+      );
+    }
+  
+    let startPage, endPage;
+    if (totalPages <= 5) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      if (page <= 3) {
+        startPage = 1;
+        endPage = 5;
+      } else if (page >= totalPages - 2) {
+        startPage = totalPages - 4;
+        endPage = totalPages;
+      } else {
+        startPage = page - 2;
+        endPage = page + 2;
+      }
+    }
+  
+    for (let i = startPage; i <= endPage; i++) {
       pages.push(
         <button
           key={i}
@@ -75,9 +166,35 @@ function App() {
         </button>
       );
     }
+  
+    if (showEllipsis && endPage < totalPages - 1) {
+      pages.push(<span key="ellipsis">...</span>);
+    }
+  
+    if (totalPages > 5 && endPage < totalPages) {
+      pages.push(
+        <button
+          key={totalPages}
+          onClick={() => setPage(totalPages)}
+          className={page === totalPages ? "active" : ""}
+        >
+          {totalPages}
+        </button>
+      );
+    }
+  
+    if (page < totalPages) {
+      pages.push(
+        <button key="next" onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      );
+    }
+  
     return pages;
   };
-
+  
+  
   const handleReverseOrder = () => {
     setReverseOrder(!reverseOrder);
   };
@@ -88,12 +205,22 @@ function App() {
     } else {
       setSortType(type);
       setSortDirection("asc");
+      setSelectedRegion("");
+      setSelectedSubregion("");
+      setResetSort(false);
     }
+  };
+
+  const handleRegionSelect = (region) => {
+    setSelectedRegion(region);
+    setSelectedSubregion("");
+    setShowRegions(false);
   };
 
   return (
     <div className="container">
       <input
+        className="search"
         type="text"
         placeholder="Search"
         value={searchTerm}
@@ -109,13 +236,22 @@ function App() {
         >
           Sort А-Я
         </button>
-        <button
-          onClick={() => handleSort("region")}
-          className={sortType === "region" ? "active" : ""}
-        >
-          Sort by Region
+        <button onClick={() => setShowRegions(!showRegions)}>
+          Show Regions
         </button>
-        <button onClick={() => setSortType("")}>Reset Sort</button>
+        {showRegions && <RegionList />}
+        <button
+          onClick={() => {
+            setSortType("");
+            setSortDirection("asc");
+            setSelectedRegion("");
+            setSelectedSubregion("");
+            setResetSort(true);
+          }}
+          className={resetSort ? "active" : ""}
+        >
+          Reset Sort
+        </button>
       </div>
       <div className="list">
         <strong>Country list:</strong>
--- src/CountryDetailsAll.jsx
+++ src/CountryDetailsAll.jsx
@@ -4,6 +4,8 @@ const CountryDetailsAll = ({ country }) => {
   const currencies = Object.values(country.currencies).map(
     (currency) => `${currency.name} (${currency.symbol})`
   );
+  const languages = Object.values(country.languages).join(", ");
+
   return (
     <div className="country-detailsAll">
       <div className="namesize">
@@ -18,6 +20,14 @@ const CountryDetailsAll = ({ country }) => {
             alt={country.name.common}
           />
         </div>
+        <div className="map-container">
+          <iframe
+            className="country-map"
+            title="Country Map"
+            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyA9u_SH77owUOj7yjOKrp8jZpuRKoS4WmA&q=${country.name.common}`}
+            allowFullScreen
+          ></iframe>
+        </div>
         <div className="flex">
           <strong>Coat of Arms</strong>
           <img
@@ -27,35 +37,46 @@ const CountryDetailsAll = ({ country }) => {
           />
         </div>
       </div>
-      <div>
-        <p>
-          <strong>Capital:</strong> {country.capital?.[0] ?? "N/A"}
-        </p>
-        <p>
-          <strong>Region:</strong> {country.region ?? "N/A"}
-        </p>
-        <p>
-          <strong>Population:</strong>{" "}
-          {country.population?.toLocaleString() ?? "N/A"}
-        </p>
-        <p>
-          <strong>Area: </strong>
-          {country.area ?? "N/A"}
-        </p>
-        <p>
-          <strong>Continents: </strong>
-          {country.continents ?? "N/A"}
-        </p>
-        <p>
-          <strong>Traffic side: </strong>
-          {country.car.side ?? "N/A"}
-        </p>
-        <p>
-          <strong>Currencie:</strong> {currencies.join(", ") ?? "N/A"}
-        </p>
+      <div className="wrap-country">
+        <div className="left-details">
+          <p>
+            <strong>Capital:</strong> {country.capital?.[0] ?? "N/A"}
+          </p>
+          <p>
+            <strong>Region:</strong> {country.region ?? "N/A"}
+          </p>
+          <p>
+            <strong>Subregion:</strong> {country.subregion ?? "N/A"}
+          </p>
+          <p>
+            <strong>Population:</strong>{" "}
+            {country.population?.toLocaleString() ?? "N/A"}
+          </p>
+          <p>
+            <strong>Area: </strong>
+            {country.area ?? "N/A"}
+          </p>
+        </div>
+        <div className="left-details">
+          <p>
+            <strong>Continents: </strong>
+            {country.continents ?? "N/A"}
+          </p>
+          <p>
+            <strong>Traffic side: </strong>
+            {country.car.side ?? "N/A"}
+          </p>
+          <p>
+            <strong>Currencies:</strong> {currencies.join(", ") ?? "N/A"}
+          </p>
+          <p>
+            <strong>Languages:</strong> {languages ?? "N/A"}
+          </p>
+        </div>
       </div>
       <button>Back</button>
     </div>
   );
 };
+
 export default CountryDetailsAll;
